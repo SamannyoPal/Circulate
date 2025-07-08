92154658324d8b64baadb562e91c2eae38767079
Samannyo Pal <samannyopal04@gmail.com>
2025-07-08 23:04:56 +0530

    handle global api calls

--- circulate_frontend/app/page.tsx
+++ circulate_frontend/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/components/button";
 import { useState } from "react";
 import { Loader2 } from "lucide-react";
 
