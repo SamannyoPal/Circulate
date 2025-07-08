0ea472cf67a76704e26365ba55df2f5247664dc8
Samannyo Pal <samannyopal04@gmail.com>
2025-07-08 22:31:57 +0530

    frontend boilerplate

---
+++ circulate_frontend/app/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Loader2 } from "lucide-react";
+
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  };
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Button isLoading={isLoading} onClick={handleClick}>
+        click me
+      </Button>
+    </div>
+  );
+}
